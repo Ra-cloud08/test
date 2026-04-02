ae668bff3b8e797778c8eb3041f98d1100372b88
Theara <sokthearathoun60@gmail.com>
2026-04-02 21:09:28 +0700

    test

--- Bassic/02_Input Output.cpp
+++ Bassic/02_Input Output.cpp
@@ -1,26 +1,50 @@
 #include<iostream>
+#include<string.h>
+// int main1(){
 
-int main1(){
+//     int a, b;
+//     std::cout<<"Enter number a: ";
+//     std::cin>>a;
+//     std::cout<<"Enter number b: ";
+//     std::cin>>b;
+//     int sum = a+b;
+//     std::cout<<"The result is: "<<sum<<std::endl;
+//     return 0;
 
-    int a, b;
-    std::cout<<"Enter number a: ";
-    std::cin>>a;
-    std::cout<<"Enter number b: ";
-    std::cin>>b;
-    int sum = a+b;
-    std::cout<<"The result is: "<<sum<<std::endl;
-    return 0;
+// }
+int main(){
+    char myarray[30], myarray1[30];
+    char myarray2[30], myarray3[30];
+    int length, i=0;
+//change first and second character A and B
+    std::cout<<"Please input a string: ";
+    std::cin>>myarray;
+    length = strlen(myarray);
+    strcpy(myarray1, myarray);
+    myarray1[0] = 'A';
+    myarray1[1] = 'B';
+    std::cout<<"1. Output: "<<myarray1<<std::endl;
 
+//change 'z' to character first thirth fith 
+    strcpy(myarray2, myarray);
+    for(i=0; i<length; i++){
+        if(i % 2 == 0){
+            myarray2[i] = 'z';
+        }
+    }
+    std::cout<<"2. Output: "<<myarray2<<std::endl;
+
+//change 'z' to character first thirth fith 
+    strcpy(myarray3, myarray);
+    for(i=0; i<length; i++){
+        if(i % 2 != 0){
+            myarray3[i] = 'x';
+        }
+    }
+    std::cout<<"3. Output: "<<myarray3<<std::endl;
+
+
+    
+    return 1;
 }
-int myFunction(){
-
-}
-class MyClass{
-
-};
-float my_num(){
-
-}
-const int MY_NAME=3;
-
 
