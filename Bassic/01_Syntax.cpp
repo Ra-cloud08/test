94fa34290ce5f8c06123041b59cea32cfe31ce86
Ra-cloud08 <thounsoktheara60@gmail.com>
2026-04-21 21:26:34 +0700

    CPP_CNXA_COST

--- Bassic/01_Syntax.cpp
+++ Bassic/01_Syntax.cpp
@@ -1,5 +1,58 @@
 #include<iostream>
+int n =20;
 int main(){
-    int
+     std::cout<<std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << "2 * " << i << " = " << (2 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+     for (int i = 1; i <= 10; ++i) {
+        std::cout << "3 * " << i << " = " << (3 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+     for (int i = 1; i <= 10; ++i) {
+        std::cout << "4 * " << i << " = " << (4 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+     for (int i = 1; i <= 10; ++i) {
+        std::cout << "5 * " << i << " = " << (5 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+     for (int i = 1; i <= 10; ++i) {
+        std::cout << "6 * " << i << " = " << (6 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+     for (int i = 1; i <= 10; ++i) {
+        std::cout << "7 * " << i << " = " << (7 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+     for (int i = 1; i <= 10; ++i) {
+        std::cout << "8 * " << i << " = " << (8 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << "9 * " << i << " = " << (9 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    std::cout<<"=============================="<<std::endl;
+    std::cout<<std::endl;
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << "10 * " << i << " = " << (10 * i) << std::endl;
+    }
+    std::cout<<std::endl;
+    
     return 0;
 }
